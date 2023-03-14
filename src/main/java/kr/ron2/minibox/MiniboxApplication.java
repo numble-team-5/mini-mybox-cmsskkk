5fd8b818f118f3fd4f5a137f1d9061bfff6e8264
Minseok-Choi <cms05041@gmail.com>
2023-03-14 14:34:21 +0900

    build: 프로젝트 기본 셋팅

---
+++ src/main/java/kr/ron2/minibox/MiniboxApplication.java
@@ -0,0 +1,13 @@
+package kr.ron2.minibox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ron2/minibox/MiniboxApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ron2.minibox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
