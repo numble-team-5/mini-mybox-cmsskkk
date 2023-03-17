f68b2b90270fe06e2e2a647b48934114ada0f37a
Minseok-Choi <cms05041@gmail.com>
2023-03-17 16:55:50 +0900

    feat: TransactionManager 등록

--- src/main/java/kr/ron2/minibox/common/config/MongoDBConfig.java
+++ src/main/java/kr/ron2/minibox/common/config/MongoDBConfig.java
@@ -1,10 +1,19 @@
 package kr.ron2.minibox.common.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @EnableMongoRepositories(basePackages = "kr.ron2.minibox")
 public class MongoDBConfig {
+
+    @Bean
+    PlatformTransactionManager mongoTransactionManager(MongoDatabaseFactory factory) {
+        return new MongoTransactionManager(factory);
+    }
 
 }
